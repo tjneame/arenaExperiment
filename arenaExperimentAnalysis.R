bff5f4de0725ab3a76b1c2e68c71242a288e9d84
Tobyn Neame <tjneame@gmail.com>
2023-09-27 14:26:54 -0600

    New linear models and scale set for figure

--- arenaExperimentAnalysis.R
+++ arenaExperimentAnalysis.R
@@ -1,6 +1,6 @@
 ################################\
 #Arena experiment analysis
-#TN and SR 2023
+#TN 2023
 ################################\
 
 #Set up-------------------------
@@ -21,7 +21,8 @@ aeDat<-aeDat%>%
   mutate(initialTwo=as_factor(initialTwo))%>%
   mutate(catSize=ordered(as_factor(catSize)))%>%
   mutate(catSize=fct_relevel(catSize,c("SS","SM","LM","LL")))%>%
-  mutate(catSize=as.numeric(catSize))
+  mutate(catSize=as.numeric(catSize))#%>%
+  #mutate(catSize=catSize-1) #Do this for multinomial regression
 
 #add some new columns and change some old ones
 aeDat<-aeDat%>%
@@ -58,40 +59,68 @@ aeGAM2<-gam(aeForm2,
             data = aeDat,
             method = 'REML')
 
+aeForm4<-as.formula(catSize~elytraLength)
+aeGAM4<-gam(aeForm4,
+            family = ocat(R=4),
+            data = aeDat,
+            method = 'REML')
+summary(aeGAM4)
+
+aeForm5<-as.formula(catSize~s(elytraLength)+difDeg)
+aeGAM5<-gam(aeForm5,
+            family = ocat(R=4),
+            data = aeDat,
+            method = 'REML')
+summary(aeGAM5)
+
 #Test models ----------------------------------------------
 ##########################################################\
 #Check that the models fit properly
-gam.check(aeGAM1) #a little weird, try something else
-plot(simulateResiduals(aeGAM1))#That looks better
+gam.check(aeGAM4) #a little weird, try something else
+plot(simulateResiduals(aeGAM4))#That looks better
 
-gam.check(aeGAM2) #a little weird, try something else
-plot(simulateResiduals(aeGAM2))#That looks better
+gam.check(aeGAM5) #a little weird, try something else
+plot(simulateResiduals(aeGAM5))#That looks better
 
 #Let's look at the summaries
-summary(aeGAM1)
+summary(aeGAM4)
 #elytra length is minimally significant for the size of caterpillar that got eaten
-summary(aeGAM2)
+summary(aeGAM5)
 #elytra length is minimally significant for the size of caterpillar that got eaten
 #AND the direction the beetle was facing did not affect the size of caterpillar that was eaten
 
-AIC(aeGAM1, aeGAM2)
-#aeGAM1 has a lower AIC, 
+AIC(aeGAM4, aeGAM5)
+#aeGAM4 has a lower AIC, 
 #and considering that the direction the beetle faced had no significant effect I will move forward with aeGAM1
 
+#Get p-Values for all lines - doesn't work -----------------------------
+#TN
+###########################################################\
+library(emmeans)
+em<-emmeans(aeGAM4, ~ 'elytraLength') #Doesn't work
+em
+#lets try making a multinomial model (make sure to change aeDat in setup)
+aeForm3<-as.formula(catSize~s(elytraLength))
+
+aeGAM3<-gam(list(catSize~elytraLength, ~elytraLength, ~elytraLength),
+            family = multinom(K=3),
+            data = aeDat,
+            method = 'REML')
+summary(aeGAM3)
 
 #Visualize ------------------------------------------------
 #Written by Sam Robinson
 ###########################################################\
 
-#aeGAM1
+#aeGAM4
 expand.grid(elytraLength=seq(7.7,10.3,0.1)) %>%
-  bind_cols(.,predict(aeGAM1,newdata=.,type='response')) %>%
+  bind_cols(.,predict(aeGAM4,newdata=.,type='response')) %>%
   setNames(c('elytraLength','SS','SM','LM','LL')) %>%
   pivot_longer(c(-elytraLength),names_to = 'size',values_to='prob') %>%
   ggplot(aes(x=elytraLength,y=prob,col=size))+geom_line()
 
 expand.grid(elytraLength=seq(7.6,10.3,0.1)) %>%
-  cbind(predict(aeGAM1,newdata=.,type='response',se=TRUE)) %>%
+  cbind(predict(aeGAM4,newdata=.,type='response',se=TRUE)) %>%
   setNames(c('elytraLength','SS','SM','LM','LL',
              'SS_se','SM_se','LM_se','LL_se')) %>%
   pivot_longer(c(-elytraLength),names_to = 'size',values_to='prob') %>%
@@ -106,19 +135,20 @@ expand.grid(elytraLength=seq(7.6,10.3,0.1)) %>%
   ylab("Mean Predation Probability")+
   geom_rug(data=aeDat, aes(x=elytraLength), alpha=0.3)+
   scale_colour_viridis_d(aesthetics = c('fill','colour'), option = "D", begin = 0.25, end = 0.85, 
-                      labels=c("Largest", "Large", "Small", "Smallest"),
-                      name='Size of Prey')+
+                      labels=c("4 (Largest)", "3", "2", "1 (Smallest)"),
+                      name='Size Class of Prey')+
   theme_bw()
-ggsave('./figures/AEResults.png', width=10, height=5)
-#aeGAM2
+ggsave('./figures/AEResults.png', width=10, height=5, scale=0.8)
+
+#aeGAM5
 expand.grid(elytraLength=seq(7.7,10.3,0.1),difDeg='45') %>%
-  bind_cols(.,predict(aeGAM2,newdata=.,type='response')) %>%
+  bind_cols(.,predict(aeGAM5,newdata=.,type='response')) %>%
   setNames(c('elytraLength','difDeg','SS','SM','LM','LL')) %>%
   pivot_longer(c(-elytraLength,-difDeg),names_to = 'size',values_to='prob') %>%
   ggplot(aes(x=elytraLength,y=prob,col=size))+geom_line()
 
 expand.grid(elytraLength=seq(7.6,10.3,0.1),difDeg='45') %>%
-  cbind(predict(aeGAM2,newdata=.,type='response',se=TRUE)) %>%
+  cbind(predict(aeGAM5,newdata=.,type='response',se=TRUE)) %>%
   setNames(c('elytraLength','difDeg','SS','SM','LM','LL',
              'SS_se','SM_se','LM_se','LL_se')) %>%
   pivot_longer(c(-elytraLength,-difDeg),names_to = 'size',values_to='prob') %>%
